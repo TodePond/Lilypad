15c3b6c000064f2f89d8526a4e4268383ebb4b77
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-07-24 12:52:31 +0100

    tailwind theme

--- apps/exquisitarium/src/App.tsx
+++ apps/exquisitarium/src/App.tsx
@@ -1,7 +1,8 @@
 export default function App() {
 	return (
-		<div className="h-screen w-screen items-center justify-center">
-			<h1 className="text-4xl text-center font-bold">Hello world!</h1>
+		<div className="p-xl flex-col">
+			<h1>EXQUISITARIUM</h1>
+			<p className="text-green">collaborative creatures</p>
 		</div>
 	)
 }
